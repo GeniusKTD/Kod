cc4e3ee585e5e89beabe0c92112bc8eec8edcd72
GeniusKTD <kdgenius@protonmail.com>
2024-12-21 19:27:12 +0100

    cinamon es xfce folytatasa

--- cinnamon_install.sh
+++ cinnamon_install.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
 set -e
+
+# Cinnamon alapcsomagok telepítése
+sudo pacman -Sy cinnamon nemo-fileroller cinnamon-translations
+
+# LightDM elindítása
+sudo systemctl start lightdm.service
+sudo systemctl enable lightdm.service
--- xfce_install.sh
+++ xfce_install.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
 set -e
+
+# XFCE alapcsomagok telepítése
+sudo pacman -Sy xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
+
+# LightDM elindítása
+sudo systemctl start lightdm.service
+sudo systemctl enable lightdm.service
