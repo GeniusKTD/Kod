ce03871e3a4da408ffaed7ad4cfc56dd1a757438
GeniusKTD <kdgenius@protonmail.com>
2024-12-21 01:09:21 +0100

    lv1+

--- install_desktop.sh
+++ install_desktop.sh
@@ -4,4 +4,4 @@ set -e
 # Alapcsomagok telepitese
 
 sudo pacman -Syu
-sudo pacman -S bash-completion git wget base base-devel mc gvfs ntfs-3g zenity
+sudo pacman -Sy bash-completion git wget base base-devel mc gvfs ntfs-3g zenity
