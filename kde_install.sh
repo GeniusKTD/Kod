fcff62016d45c2d6016bf15ddf3984bbbf163100
GeniusKTD <kdgenius@protonmail.com>
2024-12-21 19:21:45 +0100

    kde folytatas

--- kde_install.sh
+++ kde_install.sh
@@ -2,3 +2,9 @@
 set -e
 
 # KDE alapcsomagok telepitese
+
+sudo pacman -Sy plasma-desktop plasma-wayland-session kde-applications sddm sddm-kcm
+
+# SDDM elindítása
+sudo systemctl start sddm.service
+sudo systemctl enable sddm.service
