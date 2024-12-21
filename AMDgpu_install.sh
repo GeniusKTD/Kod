fe5a0e53e30d2f027b356c19bcaccf36d9696d5a
GeniusKTD <kdgenius@protonmail.com>
2024-12-21 19:40:52 +0100

    AMDgpu driver letrehozva

---
+++ AMDgpu_install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -e
+
+# mesa telepites
+
+sudo pacman -Sy mesa lib32-mesa mesa-vdpau lib32-mesa-vdpau xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon --noconfirme --nedeed
+
+# mesa git ha kell
+# yay -Sy mesa-git
+
+# ujrainditas
+
+sudo shutdown -r now
