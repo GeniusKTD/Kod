1823d6572b26696404ccdb6f8a6f023a8a12b7ac
GeniusKTD <kdgenius@protonmail.com>
2024-12-21 21:25:10 +0100

    foltozas3

--- AMDgpu_install.sh
+++ AMDgpu_install.sh
@@ -3,7 +3,7 @@ set -e
 
 # mesa telepites
 
-sudo pacman -Sy mesa lib32-mesa mesa-vdpau lib32-mesa-vdpau xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon --noconfirme --nedeed
+sudo pacman -Sy mesa lib32-mesa mesa-vdpau lib32-mesa-vdpau xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon --noconfirm --nedeed
 
 # mesa git ha kell
 # yay -Sy mesa-git
